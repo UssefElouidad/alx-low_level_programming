4d4a4b016d2b2a60f28a61172d38368f01c1913c
youssef el ouidad <y.elouidad@gmail.com>
2023-03-19 18:09:08 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - entry position
+ * return: always (success)
+ **/
 {
 		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
