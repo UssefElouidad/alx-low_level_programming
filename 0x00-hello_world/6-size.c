67973d283dbbd128a76cd60acdb56ad323841fbf
youssef el ouidad <y.elouidad@gmail.com>
2023-03-20 17:08:41 -0700

    6 size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - enrty point
+ *
+ * return : always 0 (success)
+ *
+ */
+int main(void)
+{
+	printf("Size of a char :%d\n",sizeof(char),"byte(s)");
+	printf("size of an int :%d\n",sizeof(int),"byte(s)");
+	printf("size of a long int :%d\n",sizeof(long int),"byte(s)");
+	printf("size of a float :%d\n",sizeof(float),"byte(s)");
+	return (0);
+}
