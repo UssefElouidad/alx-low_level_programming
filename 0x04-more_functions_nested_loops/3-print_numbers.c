00d737c5104a4470887a2c9b1a87594070853032
youssef el ouidad <y.elouidad@gmail.com>
2023-03-24 16:16:37 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _putchar - print the final value.
+ *
+ * Return: numbers form 0 to 9.
+ */
+void print_numbers(void)
+
+{
+	int i;
+		for(i = 0; i <= 9; i++)
+		{
+	_putchar (i + '0');
+		}
+	_putchar ('\n');
+}
