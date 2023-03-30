b0686a9d13dcb1b555521ec988273a4f7f13ae8a
youssef el ouidad <y.elouidad@gmail.com>
2023-03-29 18:21:02 -0700

    tring toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change all lowercqse of a string to upercase.
+ * @str: analized string.
+ * Return: string with uupercase letters.
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		{
+			*(str + i) = *(str + i) - 32;
+		}
+		i++;
+	}
+	return (0);
+}
