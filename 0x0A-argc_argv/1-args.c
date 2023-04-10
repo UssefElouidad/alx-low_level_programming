b5999edb0ea3c9cbc14b17c3767fc6f9ad1447a4
youssef el ouidad <y.elouidad@gmail.com>
2023-04-10 15:21:54 -0700

    silence is argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * @argc: argument cout.
+ * @argv: argument vit.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
