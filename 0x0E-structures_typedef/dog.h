3d9118ee5311e6a4889c0a53839182aa7a99d2ac
youssef el ouidad <y.elouidad@gmail.com>
2023-04-18 12:47:37 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-/*
- * struct dog - dog's basic information.
+/**
+ * struct dog - is a dog's basic information.
  * @name: first member.
  * @age: second member.
  * @owner: third member.
