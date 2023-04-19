2584031f5dc5d1b01f99914d96421557777482b5
youssef el ouidad <y.elouidad@gmail.com>
2023-04-18 17:43:56 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - a function that initialize a virable struct dog
+ * @d: pointer.
+ * @name: dog's name.
+ * @age: dog's age.
+ * @owner: dog's owner name.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
