df5264fb17da531e60a74408a950ecd886b0589e
youssef el ouidad <y.elouidad@gmail.com>
2023-06-09 12:44:41 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include"main.h"
+int sqrt_define(int n, int i);
+/**
+ * sqrt_define - a function that helps in finding the sqr root number.
+ * @i : iterate number.
+ * @n : the number in question of sqr root.
+ * Return: the result
+ */
+int sqrt_define(int n, int i)
+{
+	int mid = i * i;
+
+	if (mid > n)
+		return (-1);
+
+	else if (mid == n)
+		return (i);
+
+	return (sqrt_define(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - a fuction that returns the natural square root of a number.
+ * @n : the number to calculate score for .
+ * Return : the natural square root or -1 if dosent exist.
+ */
+int _sqrt_recursion(int n)
+{
+	return(sqrt_define(n, 1));
+}
