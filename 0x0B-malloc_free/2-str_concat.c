e25c4c0edded51eaf9494c0c654607dd37dfda6d
youssef el ouidad <y.elouidad@gmail.com>
2023-06-14 23:54:17 +0000

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
-#include"main.h"
+#include "main.h"
+#include <stdlib.h>
 /**
  * str_concat - a function that concatenate two strings
  * @s1: first string.
@@ -6,23 +7,31 @@
  * Return: NULL on failure.
  */
 
-char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2)
 {
-	if (size == o)
+	unsigned int lena = 0, lenb = 0;
+	unsigned int i, j;
+	char *conc;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[lena] != '\0')
+		lena++;
+
+	while (s2[lenb] != '\0')
+		lenb++;
+
+	conc = malloc((lena + lenb + 1) * sizeof(char));
+	if (conc == NULL)
 		return (NULL);
+	for (i = 0; i < lena; i++)
+		conc[i] = s1[i];
+	for (j = 0; j < lenb; j++)
+		conc[i++] = s2[j];
+	conc[i] = '\0';
 
-	char *array;
-	unsigned int i;
-
-	*array = malloc ((size + 1) * sizeof(char));
-
-	if (array == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		array[i] = c;
-
-	array[size] = '\0';
-
-	return (array);
+	return (conc);
 }
