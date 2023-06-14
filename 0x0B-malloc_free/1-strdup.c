ca8fc7a19917f95612eb950a89f3dadb7fb0c61d
youssef el ouidad <y.elouidad@gmail.com>
2023-06-14 22:51:05 +0000

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - a function that returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the string given as a parameter
+ * Return: returns a pointer to a new string, NULL on failure.
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int lentgh;
+	unsigned int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[lentgh] != '\0')
+		lentgh++;
+
+	duplicate = malloc((lentgh + 1) * sizeof(char));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	for (i = 0; i < lentgh; i++)
+		duplicate[i] = str[i];
+
+	duplicate[lentgh] = '\0';
+
+		return (duplicate);
+		free(duplicate);
+}
+
+
+
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/**
+ * str_concat - a function that concatenate two strings
+ * @s1: first string.
+ * @s2: second string.
+ * Return: NULL on failure.
+ */
+
+char *str_concat(char *s1, char *s2);
+{
+	if (size == o)
+		return (NULL);
+
+	char *array;
+	unsigned int i;
+
+	*array = malloc ((size + 1) * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	array[size] = '\0';
+
+	return (array);
+}
