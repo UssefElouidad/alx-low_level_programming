e57976a4fa6969fc80f82b4a6212cfec6619030c
youssef el ouidad <y.elouidad@gmail.com>
2023-06-15 22:36:08 +0000

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int multiply_numbers(char* num1, char* num2)
+{
+    int result;
+    char* endptr;
+
+    result = strtol(num1, &endptr, 10) * strtol(num2, &endptr, 10);
+    return result;
+}
+
+int main(int argc, char* argv[])
+{
+	char* num1;
+	char* num2;
+	int result;
+
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 98;
+    }
+
+    num1 = argv[1];
+    num2 = argv[2];
+
+    result = multiply_numbers(num1, num2);
+    printf("%d\n", result);
+
+    return 0;
+}
+
