616ee18711821f0bdff79e06bb88ca6781401910
youssef el ouidad <y.elouidad@gmail.com>
2023-06-20 00:11:30 +0000

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Define a new type struct dog
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: the owner's name.
+ */
 struct dog
 {
 	char *name;
