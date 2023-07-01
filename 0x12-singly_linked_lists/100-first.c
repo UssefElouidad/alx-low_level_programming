3b91e1cd130a01a74d6f0e1001a65a259998f242
youssef el ouidad <y.elouidad@gmail.com>
2023-07-01 02:10:21 +0100

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * before_main - a function that printd
+ * You're beat! and yet, you must allow,\nI
+ * bore my house upon my back!\n before the main function is executed.
+ */
 void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
