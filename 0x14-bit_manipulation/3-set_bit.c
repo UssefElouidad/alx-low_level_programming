5d33f874202592ba7655ee082086b105cd628270
youssef el ouidad <y.elouidad@gmail.com>
2023-07-07 23:36:41 +0100

    3.11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,12 +7,10 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
 
 	if (index >= sizeof(unsigned int) * 8)
 		return (-1);
 
-	mask = 1UL << index;
-	*n |= mask;
-	return (-1);
+	*n |= 1UL << index;
+	return (1);
 }
