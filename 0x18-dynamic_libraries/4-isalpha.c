1f04e2a4212e1f36f237ccfb47deacaa15051d3a
youssef el ouidad <y.elouidad@gmail.com>
2023-08-21 16:55:19 +0100

    A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha: checks for alphabetic characters.
+ * if the input is character it  showes: 1
+ * otherwise it shows 0
+ * @c: the character in ASCII code
+ * Return: 1 for alphabetic charracter 0 for the rest.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
