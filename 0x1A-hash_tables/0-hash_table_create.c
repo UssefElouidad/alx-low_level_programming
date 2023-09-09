f0ba6feecf5c73dce4a41504c4eea74eb0cc9f96
youssef el ouidad <y.elouidad@gmail.com>
2023-09-09 01:24:05 +0100

    0. >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,24 +8,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *n_table = NULL;
-    unsigned long int i;
+	hash_table_t *n_table = NULL;
+	unsigned long int i;
 
-    n_table = malloc(sizeof(hash_table_t));
-    if (n_table == NULL)
-        return (NULL);
+	n_table = malloc(sizeof(hash_table_t));
+	if (n_table == NULL)
+		return (NULL);
 
-    n_table->size = size;
-    n_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (n_table->array == NULL)
-    {
-        free(n_table);
-        return (NULL);
-    }
+	n_table->size = size;
+	n_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (n_table->array == NULL)
+	{
+		free(n_table);
+		return (NULL);
+	}
 
-    for (i = 0; i < size; i++)
-        n_table->array[i] = NULL;
+	for (i = 0; i < size; i++)
+		n_table->array[i] = NULL;
 
-    return (n_table);
+	return (n_table);
 }
 
