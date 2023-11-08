f0cb9b38106594919f4354bb25f43ec894dbd503
youssef el ouidad <y.elouidad@gmail.com>
2023-11-08 21:11:44 +0100

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,10 +18,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	idx = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[idx];
 	while (tmp != NULL)
-		{
-			if (strcmp(tmp->key, key) == 0)
-				return (tmp->value);
-			tmp = tmp->next;
-		}
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
 	return (NULL);
 }
